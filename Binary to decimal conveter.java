25b0fca0f4ae4914ebeb5826386f47c72b3f3511
sivashoney <98447531+sivashoney@users.noreply.github.com>
2023-10-14 16:10:27 +0530

    Add files via upload

---
+++ Binary to decimal conveter.java
@@ -0,0 +1,7 @@
+public class Main{  
+public static void main(String args[]){  
+System.out.println(Integer.parseInt("1010",2));  
+System.out.println(Integer.parseInt("10101",2));  
+System.out.println(Integer.parseInt("11111",2));  
+ }
+}  
